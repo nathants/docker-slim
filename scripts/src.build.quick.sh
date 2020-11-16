4104d0ad46ef2f8bf6facd06530f054aecfa7ee8
nathants <me@nathants.com>
2020-11-16 10:37:56 -0800

    quick build sensor

--- scripts/src.build.quick.sh
+++ scripts/src.build.quick.sh
@@ -7,4 +7,6 @@ while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 BDIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
 mkdir -p ${BDIR}/dist_linux/
+rm -f ${BDIR}/dist_linux/*
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=vendor -o "${BDIR}/dist_linux/docker-slim" "${BDIR}/cmd/docker-slim/main.go"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=vendor -o "${BDIR}/dist_linux/docker-slim-sensor" "${BDIR}/cmd/docker-slim-sensor/main.go"
